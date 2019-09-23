5459378ba685a92a49c8357445a669c77290036c
ashish-jabble <ashish@dianomic.com>
2019-09-23 12:55:02 +0530

    iteration 9

--- requirements.sh
+++ requirements.sh
@@ -24,6 +24,7 @@ set -e
 foglamp_location=`pwd`
 os_name=`(grep -o '^NAME=.*' /etc/os-release | cut -f2 -d\" | sed 's/"//g')`
 os_version=`(grep -o '^VERSION_ID=.*' /etc/os-release | cut -f2 -d\" | sed 's/"//g')`
+
 echo "Platform is ${os_name}, Version: ${os_version}"
 
 if [[ ( $os_name == *"Red Hat"* || $os_name == *"CentOS"* ) &&  $os_version == *"7"* ]]; then
